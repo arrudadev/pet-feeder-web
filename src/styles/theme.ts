a7de818f2873bef0ff99dc43df60bad858cdbfb2
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-11-10 18:56:27 -0300

    style: add custom colors in theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,4 +5,16 @@ export const theme = extendTheme({
     heading: 'Roboto',
     body: 'Roboto',
   },
+  colors: {
+    green: {
+      '600': '#2B7A4B',
+      '500': '#32B768',
+      '400': '#DAF2E4',
+    },
+    gray: {
+      '600': '#52665A',
+      '500': '#CFCFCF',
+      '400': '#F0F0F0',
+    },
+  },
 });
