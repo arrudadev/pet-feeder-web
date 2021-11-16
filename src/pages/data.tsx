8fce84dd026c27e42a6f8101b9fcf0b276b0791a
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2021-11-15 22:54:35 -0200

    chore: disable data page

--- src/pages/data.tsx
+++ src/pages/data.tsx
@@ -1,30 +1,33 @@
 import type { NextPage } from 'next';
 
-import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 
-import { Header } from '../components/Header';
-import { Sidebar } from '../components/Sidebar';
-import { DayliControl } from '../components/DayliControl';
+// import { Header } from '../components/Header';
+// import { Sidebar } from '../components/Sidebar';
+// import { DayliControl } from '../components/DayliControl';
 
 const Data: NextPage = () => {
   return (
-    <Flex direction="column">
-      <Header isLoggedIn />
+    <Text>
+      Data page disabled
+    </Text>
+    // <Flex direction="column">
+    //   <Header isLoggedIn />
 
-      <Flex w="100%" h="100%" my="6" maxW={1480} mx="auto" px="6">
-        <Sidebar />
+    //   <Flex w="100%" h="100%" my="6" maxW={1480} mx="auto" px="6">
+    //     <Sidebar />
 
-        <Box flex="1" w="100%">
-          <Flex>
-            <Heading>Todos os Dados</Heading>
-          </Flex>
+    //     <Box flex="1" w="100%">
+    //       <Flex>
+    //         <Heading>Todos os Dados</Heading>
+    //       </Flex>
 
-          <Box mt="20px">
-            <DayliControl useAllColumns />
-          </Box>
-        </Box>
-      </Flex>      
-    </Flex>
+    //       <Box mt="20px">
+    //         <DayliControl useAllColumns />
+    //       </Box>
+    //     </Box>
+    //   </Flex>      
+    // </Flex>
   );
 };
 
