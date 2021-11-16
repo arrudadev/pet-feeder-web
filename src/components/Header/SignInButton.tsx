9f3eec3e1ef48d84f8060c7a9916f2c3845cc208
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2021-11-16 02:00:36 -0200

    chore: add google authentication

--- src/components/Header/SignInButton.tsx
+++ src/components/Header/SignInButton.tsx
@@ -1,6 +1,26 @@
+import { useRouter } from "next/router";
+
 import { Button, Image } from '@chakra-ui/react';
 
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+import { auth } from '../../services/firebase';
+
+const provider = new GoogleAuthProvider();
+
 export const SignInButton: React.FC = () => {
+  const router = useRouter();
+
+  const handleSignIn = async () => {
+    const response = await signInWithPopup(auth, provider);
+
+    const googleAccount = response.user;
+
+    console.log(googleAccount);
+
+    router.push('/dashboard');
+  }
+
   return (
     <Button
       display="flex"
@@ -13,6 +33,7 @@ export const SignInButton: React.FC = () => {
       borderRadius="3rem"
       border="0"
       p="0 1.5rem"
+      onClick={() => handleSignIn()}
     >
       <Image
         src="/icons/google-icon.png"
